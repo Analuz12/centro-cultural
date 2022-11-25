bdb1f9466553abc2290ae06463d08969a4066dfb
Ana luz Biolatti <anluz_1420@hotmail.com>
2022-11-25 08:25:52 -0300

    formulario

--- form.js
+++ form.js
@@ -1,10 +1,41 @@
-var inputs = document.getElementsByClassName('formulario__input');
-for (var i = 0; i < inputs.length; i++){
-    inputs [i].addEventListener('Keyup', function(){
-        if(this.value.length>=1) {
-            this.nextElementSibling.classList.add('fijar');
-        }else{
-            this.nextElementSibling.classList.remove('fijar');
-        }
-    });
+let nameError= document.getElementById('name-error');
+let phoneError= document.getElementById('phone-error');
+let emailError= document.getElementById('email-error');
+let messageError= document.getElementById('message-error');
+let submitError= document.getElementById('submit-error');
+
+/*Esta funcion se ejecutara cada ves que empezemos a escribir en los campos*/
+function validateName(){
+    var name =document.getElementById('contact-name').value;
+    if(name.lenght == 0){
+        nameError.innerHTML ='Se requiere el nombre';
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]\s{1}[A-Za-z]&/)){
+        nameError.innerHTML ='Escriba el Apellido y nombre completo';
+        return false;
+    }
+
+    nameError.innerHTML = '<i class="fa-sharp fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+
+function validatePhone(){
+    let phone =document.getElementById('contact-phone').value;
+
+    if(phone.lenght == 0){
+        phoneError.innerHTML ='Se requiere el telefono'
+        return false;
+    }
+    if(phone.lenght !== 10){
+        phoneError.innerHTML= 'El numero ingresado es invalido'
+
+    }
+    if(!phone.match(/^[0-9]{10}$/)){
+        phoneError.innerHTML ='Solo ingrese numeros';
+        return false;
+    }
+    phoneError.innerHTML ='<i class="fa-sharp fa-solid fa-circle-check"></i>';
+    return true;
 }
