b0b4a61f8db740bb3acd4f51d5dbdbc4e1a07f5d
Ana luz Biolatti <anluz_1420@hotmail.com>
2022-11-28 00:05:01 -0300

    cambios

--- carousel.js
+++ carousel.js
@@ -5,14 +5,12 @@ window.onload= function(){
         'Imagenes/folclore.jpg',
         'Imagenes/piano.jpg',
     ];
-
-const TIEMPO_INTERVALO_MILESIMA_SEG = 2000;
+    
 let posicionActual =0;
 let $botonRetroceder = document.querySelector ('#retroceder');
 let $botonAvanzar = document.querySelector ('#avanzar');
 let $imagen = document.querySelector ('#imagen');
-let $botonPlay = document.querySelector ('#play');
-let $botonStop = document.querySelector ('#stop'); 
+
 
 /*funciones*/
 /*Funcion que cambia la imagen a la siguiente posicion*/
@@ -44,33 +42,9 @@ function renderizarImagen() {
     $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
 }
 
-/*Funcion para activar el autoplay de la imagenes*/
-
-function playIntervalo() {
-    intervalo = setInterval(pasarFoto,TIEMPO_INTERVALO_MILESIMA_SEG);
-    // Desactivamos los botones de control
-    $botonAvanzar.setAttribute('disabled', true);
-    $botonRetroceder.setAttribute('disabled', true);
-    $botonPlay.setAttribute('disabled', true);
-    $botonStop.removeAttribute('disabled');
-
-}
-/* Para el autoplay de la imagen*/
-
-function stopIntervalo() {
-    clearInterval(intervalo);
-    // Activamos los botones de control
-    $botonAvanzar.removeAttribute('disabled');
-    $botonRetroceder.removeAttribute('disabled');
-    $botonPlay.removeAttribute('disabled');
-    $botonStop.setAttribute('disabled', true);
-}
-
 // Eventos
 $botonAvanzar.addEventListener('click', pasarFoto);
 $botonRetroceder.addEventListener('click', retrocederFoto);
-$botonPlay.addEventListener('click', playIntervalo);
-$botonStop.addEventListener('click', stopIntervalo);
 // Iniciar
 renderizarImagen();
 } 
